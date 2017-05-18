10e801804927be5aa1c37582e6b4d6f15c017b1d
RiccardoGrin <grinovericcardo@gmail.com>
2017-05-18 20:27:28 +1000

    Created header and footer PHP files
    Added the PHP files as required on all pages and check that they worked

---
+++ header.php
@@ -0,0 +1,20 @@
+
+    <div class="header-container">
+        <div class="header">
+            <div class="container">
+                <div id="logo" onclick="window.location.href='index.php'">
+                    <div class="imgholder">
+                        <img class="img" src="images/park_logo.png" alt="Park logo">
+                    </div>
+                </div>
+                <ul class="nav">
+                    <li><a href="index.php#GoogleMap">Nearby Parks</a></li>
+                    <li><a href="index.php#featuredParks">Featured Parks</a></li>
+                    <li><a href="aboutUs.php">About</a></li>
+                    <!-- Button on front page to open signup popup form -->
+                    <li><button onclick="window.location.href='SignUp.php'" class="signupopt">Sign Up</button>
+                        <button onclick="window.location.href='Login.php'" class="loginopt">Login</button></li>
+                </ul>
+            </div>
+        </div>
+    </div>
--- individualPageTwo.php
+++ individualPageTwo.php
@@ -16,25 +16,9 @@
 	<div id="wrapper">
 
 		<!--         Header        -->
-		<div class="header-container">
-			<div class="header">
-				<div class="container">
-					<div id="logo" onclick="window.location.href='homePage.php'">
-						<div class="imgholder">
-							<img class="img" src="images/park_logo.png" alt="Park logo">
-						</div>
-					</div>
-					<ul class="nav">
-						<li><a href="homePage.php#GoogleMap">Nearby Parks</a></li>
-						<li><a href="homePage.php#featuredParks">Featured Parks</a></li>
-						<li><a href="aboutUs.php">About</a></li>
-						<!-- Button on front page to open signup popup form -->
-						<li><button onclick="window.location.href='SignUp.php'" class="signupopt">Sign Up</button>
-							<button onclick="window.location.href='Login.php'" class="loginopt">Login</button></li>
-					</ul>
-				</div>
-			</div>
-		</div>
+		<?php
+	        require "header.php";
+	    ?>
 
 
 		<!--         Search and large park background        -->
@@ -115,27 +99,9 @@
 	</div>
 
 	<!--       Footer       -->
-	<div class="section">
-		<div class="footer">
-			<div class="container white">
-				<div class="col three left">
-					<h1>Contact</h1>
-					<p>Reef Stevens - n9441409 - reef.stevens@qut.edu.au</p>
-					<p>Riccardo Grinover - n8783012 - ricardo.grinover@qut.edu.au</p>
-				</div>
-				<div class="col three left" onclick="window.location.href='aboutUs.php'" style="cursor:pointer">
-					<h1>Our Mission</h1>
-					<p>Parks are a great place to connect with people and to relax.</p>
-					<p>We aim to find the best park for you, your family and pets, anywhere in Brisbane!!</p>
-				</div>
-				<div class="col three left">
-					<h1>QUT</h1>
-					<p>This website has been developed for CAB230 2017</p>
-				</div>
-				<div class="group"></div>
-			</div>
-		</div>
-	</div>
+	<?php
+        require "footer.php";
+    ?>
 
 
 
