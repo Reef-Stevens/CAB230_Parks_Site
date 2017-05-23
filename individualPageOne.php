31f998b84445e9163b18915f1d7f3e05fc803ad7
RiccardoGrin <grinovericcardo@gmail.com>
2017-05-23 10:50:50 +1000

    individual page shows results

--- individualPageOne.php
+++ individualPageOne.php
@@ -4,17 +4,29 @@ require 'adminPermission.inc';
 ?>
 
 <body id="individualPageOne">
+	<?php
+	include('createdb.inc');
+
+	$id = $_GET['id'];
+
+	$search = $pdo->prepare("SELECT Name, Suburb, Street, Latitude, Longitude  FROM dataset WHERE id = $id");
+	// Execute with wildcards
+	$search->execute();
+	$data = $search->fetch(PDO::FETCH_ASSOC)
+
+?>
 
 	<!--         Search and large park background        -->
 	<div class="section">
 		<div class="containerMap">
-			<h1>Brisbane City Botanic Gardens</h1>
+			<h1><?php  echo $data['Name'];  ?></h1>
 			<img class="resultMap" src="images/test.png" alt="parkMap">
 		</div>
 	</div>
 
 	<div class="individualParkInfo">
-		<h2>ALICE ST	BRISBANE</h2>
+		<h2><?php  echo $data['Suburb'];  ?></h2>
+		<h2><?php  echo $data['Street'];  ?></h2>
 	</div>
 
 
